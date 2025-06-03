a826cbbf583380e1110c6d7ed5bd2416a74b1d8c
draveos <draveos20@gmail.com>
2025-06-03 14:48:34 +0900

    uploaded

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/<Components>/', // 여기에 GitHub 레포 이름 넣기
+  base: '/Components/', // 여기에 GitHub 레포 이름 넣기
   plugins: [react()],
 });
